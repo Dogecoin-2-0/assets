d099b5e4f52abe8e66335653c1e61320b189b81f
Kingsley Victor <javaprodigy56@gmail.com>
2021-12-10 13:12:49 +0100

    add BUSD

--- index.js
+++ index.js
@@ -21,18 +21,34 @@ router.get('/assets/:id/image', (req, res) => {
   );
 });
 
-router.get('/assets/bep20/:address/info', (req, res) => {
+router.get('/assets/tokens/:network/:address/info', (req, res) => {
   let result = fs.readFileSync(
     path.join(
       __dirname,
-      `/blockchains/binance/assets/${req.params.address}/info/info.json`
+      `/blockchains/${req.params.network}/assets/${req.params.address}/info.json`
     )
   );
   result = JSON.parse(result.toString());
   res.status(200).json({ result });
 });
 
+router.get('/assets/tokens/:network/:address/image', (req, res) => {
+  res.sendFile(
+    path.join(
+      __dirname,
+      `blockchains/${req.params.network}/assets/${req.params.address}/logo.png`
+    )
+  );
+});
+
+router.get('/assets/tokens/:network/addresses', (req, res) => {
+  const result = fs.readdirSync(
+    path.join(__dirname, `blockchains/${req.params.network}/assets`)
+  );
+  res.status(200).json({ result });
+});
+
 app.use(require('morgan')('dev'));
 app.use('/', router);
 
-app.listen(port, () => console.log(`App is running om port ${port}`));
+app.listen(port, () => console.log(`App is running on port ${port}`));
