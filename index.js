df33bb14dac7a6cdb5bc5eb25fbaf364379ef509
Kingsley Victor <javaprodigy56@gmail.com>
2021-12-18 23:04:03 +0100

    Add cors

--- index.js
+++ index.js
@@ -68,6 +68,12 @@ router.get('/health', (req, res) => {
 });
 
 app.use(require('morgan')('dev'));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 app.use('/', router);
 
 app.listen(port, () => console.log(`App is running on port ${port}`));
